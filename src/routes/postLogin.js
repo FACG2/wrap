284ef3860e609f365d71c513dea8026ffe3b472e
walidmash <waleedmash2@gmail.com>
2017-09-20 13:08:15 +0300

    finishing login

--- src/routes/postLogin.js
+++ src/routes/postLogin.js
@@ -2,12 +2,20 @@ const functions = require('../queries/index.js');
 const tokenHandler = require('../views/helpers/token.js');
 
 module.exports = (req, res, next) => {
-  functions.users.getUserLogIn(req.body.email, req.body.password, (err, result) => {
-    if (err) {
-      next(err);
-    } else {
-      tokenHandler.addToken(res, result.id, result.username, result.avatar);
-      res.render('dashboard.hbs');
-    }
+  let data = '';
+  req.on('data', function (chunk) {
+    data += chunk;
+  });
+  req.on('end', function () {
+    data = JSON.parse(data);
+    functions.users.getUserLogIn(data.email, data.password, (err, result) => {
+      if (err) {
+        const message = err === 'not matched' ? err : 'connection error';
+        res.send(message);
+      } else {
+        tokenHandler.addToken(res, result.id, result.username, result.avatar);
+        res.send('/ref');
+      }
+    });
   });
 };
