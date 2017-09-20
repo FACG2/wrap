b4388982770f01c1edb041c4889aa31f479732a9
walidmash <waleedmash2@gmail.com>
2017-09-20 09:53:47 +0300

    fix problem in auth middleware

--- src/views/helpers/auth.js
+++ src/views/helpers/auth.js
@@ -1,20 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+  console.log(req.url !== '/');
   if (req.url !== '/') {
     const token = req.cookies.token;
     if (!token) {
-      res.render('error.hbs', {message: 'Access is denied, Please login'});
+      return res.render('error.hbs', {message: 'Access is denied, Please login'});
     } else {
       jwt.verify(token, 'wrap shhh', function (err, user) {
         if (err) {
           res.cookie('token', '', {maxAge: 0});
-          res.render('error.hbs', {message: 'Access is denied, Please login'});
+          return res.render('error.hbs', {message: 'Access is denied, Please login'});
         } else {
           req.user = user;
-          next();
+          return next();
         }
       });
     }
   }
+    console.log('sdas');
+    next();
 };
