86c0bcf77b3b035f6e64ab005a990a262e2816b3
hana <hana@gmail.com>
2017-09-22 15:02:33 +0300

    project query function

--- src/queries/projects.js
+++ src/queries/projects.js
@@ -46,7 +46,7 @@ const getCurrentTasks = (userId, cb) => {
 
 const getCurrentProjects = (userId, cb) => {
   const sql = {
-    text: `SELECT project_id FROM user_project INNER JOIN projects ON user_project.project_id = projects.id WHERE user_project.user_id = ${userId} AND projects.finished= false`,
+    text: `SELECT project_id FROM user_project INNER JOIN projects ON user_project.project_id = projects.id WHERE user_project.user_id = $1 AND projects.finished= false`,
     values: [userId] };
 
   connection.query(sql, (err, res) => {
@@ -57,6 +57,28 @@ const getCurrentProjects = (userId, cb) => {
     }
   });
 };
+
+const getProjectDetails = (projectId, cb) => {
+  const sql = {
+    text: `SELECT projects.title,projects.progress  FROM projects WHERE projects.id = $1`,
+    values: [projectId]
+  };
+  connection.query(sql, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res);
+    }
+  });
+};
+
+getProjectDetails(2, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res);
+  }
+});
 
 const getFinishedProjects = (userId, cb) => {
   const sql = {
@@ -102,20 +124,71 @@ const FilterByPriority = (userId, cb) => {
 };
 // /// filter by deadline in the front-end
 
-const getProjectName = (taskId,cb)=>{
-  const sql ={
+const getProjectName = (taskId, cb) => {
+  const sql = {
     text: `SELECT projects.title FROM projects INNER JOIN tasks ON tasks.project_id = projects.id WHERE tasks.id = $1`,
-    values:[taskId]
-  }
-    connection.query(sql, (err, res) => {
-      if (err) {
-        cb(err);
-      } else {
-        cb(null, res);
-      }
-    });
-}
+    values: [taskId]
+  };
+  connection.query(sql, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res);
+    }
+  });
+};
 
+const getAllSprints = (projectId, cb) => {
+  const sql = {
+    text: `SELECT sprints.id, sprints.title, sprints.progress FROM sprints WHERE project_id= $1`,
+    values: [projectId] };
+  connection.query(sql, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res);
+    }
+  });
+};
+
+const getFinishedSprints = (projectId, cb) => {
+  const sql = {
+    text: `SELECT sprints.id, sprints.title, sprints.progress FROM sprints WHERE project_id= $1 AND closed != False`,
+    values: [projectId] };
+  connection.query(sql, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res);
+    }
+  });
+};
+
+const getCurrentSprints = (projectId, cb) => {
+  const sql = {
+    text: `SELECT sprints.id, sprints.title, sprints.progress FROM sprints WHERE project_id= $1 AND closed = False`,
+    values: [projectId] };
+  connection.query(sql, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res);
+    }
+  });
+};
+
+const getTasksByState = (sprintId, state, cb) => {
+  const sql = {
+    text: `SELECT * FROM tasks WHERE sprint_id= $1 AND state = $2`,
+    values: [projectId, state] };
+  connection.query(sql, (err, res) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, res);
+    }
+  });
+};
 
 module.exports = {
   getTasksByUserId,
@@ -123,5 +196,10 @@ module.exports = {
   getCurrentProjects,
   getFinishedProjects,
   getAllProjects,
-  FilterByPriority
+  FilterByPriority,
+  getAllSprints,
+  getFinishedSprints,
+  getCurrentSprints,
+  getTasksByState,
+  getProjectDetails
 };
