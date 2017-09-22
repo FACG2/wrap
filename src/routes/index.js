e246cf1852f2d53fac95f9aa7103eb825a77fff5
walidmash <waleedmash2@gmail.com>
2017-09-22 19:04:42 +0300

    project page , add task form

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const routes = require('./routes.js');
 const router = express.Router();
 
 router.get('/', routes.getHome);
+router.get('/projects/:project_id', routes.getProjectPage);
 router.post('/signUp', routes.postSignup);
 router.post('/login', routes.postLogin);
 router.post('/addProject', routes.postAddProject);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,5 +2,6 @@ module.exports = {
   getHome: require('./getHome.js'),
   postSignup: require('./postSignup.js'),
   postLogin: require('./postLogin.js'),
+  getProjectPage: require('./getProjectPage.js'),
   postAddProject: require('./postAddProject.js')
 };
