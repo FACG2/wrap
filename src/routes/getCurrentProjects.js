a60d96bc41cdefb0c227344a92dfe7c030d48628
walidmash <waleedmash2@gmail.com>
2017-09-23 13:13:55 +0300

    get user id not result id

--- src/routes/getCurrentProjects.js
+++ src/routes/getCurrentProjects.js
@@ -1,6 +1,6 @@
 const queries = require('../queries/index.js');
 module.exports = (req, res, next) => {
-  queries.project.getCurrentProjects(req.result.id, (err, res) => {
+  queries.project.getCurrentProjects(req.user.id, (err, res) => {
     if (err) {
       next(err);
     } else {
