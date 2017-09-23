a69b147489e9429844f0a046021a1c584fbd4a37
walidmash <waleedmash2@gmail.com>
2017-09-23 09:38:10 +0300

    add create project ajax tab

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,14 +1,31 @@
 (function () {
   var linkHash = window.location.hash;
   var dashboardContent = document.getElementById('dashboardContent');
+  /* Tasks Tab */
   if (linkHash === '#tasks') {
     loading(dashboardContent);
     renderTasks(dashboardContent);
   }
+  document.getElementById('tasksButton').addEventListener('click', function (event) {
+    loading(dashboardContent);
+    renderTasks(dashboardContent);
+  });
+
+  /* Create New Project Tab */
+  if (linkHash === '#createProject') {
+    loading(dashboardContent);
+    renderCreateProjectForm(dashboardContent);
+  }
+  document.getElementById('createProjectButton').addEventListener('click', function (event) {
+    loading(dashboardContent);
+    renderCreateProjectForm(dashboardContent);
+  });
 })();
+
 function loading (container) {
   container.innerHTML = '<h2>Loading.....</h2>';
 }
+
 function renderTasks (container) {
   getTasks(function (err, data) {
     if (err) {
@@ -20,12 +37,14 @@ function renderTasks (container) {
         container.innerHTML = '<h2>No tasks assigned to you for now!<h2>';
       } else {
         container.innerHTML = data.reduce(function (acc, task) {
-          return acc += '<p>task id : ' + task.id + '</p>';
+          acc += '<p>task id : ' + task.id + '</p>';
+          return acc;
         }, '');
       }
     }
   });
 }
+
 function getTasks (cb) {
   apiReq('/getUsersTasks','GET', function (err, data) {// eslint-disable-line
     if (err) {
@@ -34,4 +53,15 @@ function getTasks (cb) {
       cb(null, data);
     }
   });
+}
+
+function renderCreateProjectForm (container) {
+  container.innerHTML = '<h1>Create project</h1>' +
+                        '<form id="createProjectForm" action="/addProject" method="post">' +
+                          '<label>Title : <input type="text" name="title" value="" placeholder="Enter Project name.." required></label>' +
+                          '<label>description : <input type="text" name="description" value="..." placeholder="Enter Project description.."></label>' +
+                          '<label>Working days in a week : <input type="number" name="wDays" value="-1" placeholder="Enter Project name.."></label>' +
+                          '<label>Working hours in a day : <input type="number" name="wHours" value="-1" placeholder="Enter Project name.."></label>' +
+                          '<input type="submit" name="submit" value="Create Project!">' +
+                        '</form>';
 }
