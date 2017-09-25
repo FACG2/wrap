f4128bbae2e9a685a4533e7e73b4d07c11f88ebc
walidmash <waleedmash2@gmail.com>
2017-09-25 03:32:37 +0300

    comment the logs until finishing them

--- src/routes/helpers/logs.js
+++ src/routes/helpers/logs.js
@@ -1,19 +1,19 @@
-const queries = require('../../queries/index.js');
-
-const createProject = (username, userId, projectId, projectName, cb) => {
-  const context = `${username} Created New project: ${projectName}`;
-  const link = `/projects/${projectId}`;
-  const type = 'project';
-  const actionId = projectId;
-  queries.logs.addLog(projectId, context, type, link, actionId, username, (err, res) => {
-    if (err) {
-      cb(err);
-    } else {
-      queries.logs.addNotification();
-    }
-  });
-};
-
-module.exports = {
-  createProject
-};
+// const queries = require('../../queries/index.js');
+//
+// const createProject = (username, userId, projectId, projectName, cb) => {
+//   const context = `${username} Created New project: ${projectName}`;
+//   const link = `/projects/${projectId}`;
+//   const type = 'project';
+//   const actionId = projectId;
+//   queries.logs.addLog(projectId, context, type, link, actionId, username, (err, res) => {
+//     if (err) {
+//       cb(err);
+//     } else {
+//       queries.notifications.addNotification();
+//     }
+//   });
+// };
+//
+// module.exports = {
+//   createProject
+// };
