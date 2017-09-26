8f795ba81f81a15962dee6d1e2f0e283c373eb08
walidmash <waleedmash2@gmail.com>
2017-09-26 07:45:03 +0300

    remove the logs

--- src/routes/postCreateSprint.js
+++ src/routes/postCreateSprint.js
@@ -1,13 +1,11 @@
 const queries = require('../queries/index.js');
 module.exports = (req, res, next) => {
-  // const addSprint = (duration, projectId, cb) => {
   let data = '';
   req.on('data', function (chunk) {
     data += chunk;
   });
   req.on('end', function () {
     const durationInDays = parseInt(data);
-    console.log('wwwwwwwwwwwww',durationInDays);
     queries.sprints.addSprint(durationInDays, req.params.project_id, (err, result) => {
       if (err) {
         return next(err);
