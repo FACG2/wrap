c83a5418ef2c5858c6cacf3ea1c5252f4667c9ff
walidmash <waleedmash2@gmail.com>
2017-09-26 05:27:45 +0300

    add /backlogTasks route

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,8 @@ router.get('/finishedProjects', routes.getFinishedProjects);
 router.get('/logout', routes.getLogout);
 router.get('/stateTasks/:state_id', routes.getStateTasks);
 router.get('/projects/:project_id/currentSprint', routes.getCurrentSprint);
+router.get('/projects/:project_id/stateTasks/:state_id', routes.getStateTasks);
+router.get('/backlogTasks', routes.getBacklogTasks);
 router.post('/signUp', routes.postSignup);
 router.post('/login', routes.postLogin);
 router.post('/addProject', routes.postAddProject);
