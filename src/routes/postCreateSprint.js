a3113003fd376ce2cdec94ce67a1342c3f2c2ec2
walidmash <waleedmash2@gmail.com>
2017-09-26 07:02:40 +0300

    add the handler to create sprint

--- src/routes/postCreateSprint.js
+++ src/routes/postCreateSprint.js
@@ -1,4 +1,18 @@
 const queries = require('../queries/index.js');
 module.exports = (req, res, next) => {
-
-}
+  // const addSprint = (duration, projectId, cb) => {
+  let data = '';
+  req.on('data', function (chunk) {
+    data += chunk;
+  });
+  req.on('end', function () {
+    const durationInDays = JSON.parse(data);
+    queries.sprints.addSprint(durationInDays, req.params.project_id, (err, result) => {
+      if (err) {
+        return next(err);
+      } else {
+        res.send('/rel');
+      }
+    });
+  });
+};
