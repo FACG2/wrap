39ee5b4f4a7d90663035ed54676aa143c35d3b10
walidmash <waleedmash2@gmail.com>
2017-10-02 15:18:46 +0300

    add project name to project page side nav

--- src/routes/getProjectPage.js
+++ src/routes/getProjectPage.js
@@ -5,25 +5,32 @@ module.exports = (req, res, next) => {
   data.user = req.user;
   data.scriptName = 'project';
   data.hasCurrentSprint = false;
-  queries.sprints.getCurrentSprint(req.params.project_id, (err, result) => {
-    if (err) {
-      next(err);
+  queries.projects.getProjectNameById(req.params.project_id, (error, res1) => {
+    if (error) {
+      next(error);
     } else {
-      if (result) {
-        data.hasCurrentSprint = true;
-        queries.sprints.getSprintStates(result.id, (err2, result2) => {
-          if (err2) {
-            next(err);
+      data.projectName = res1;
+      queries.sprints.getCurrentSprint(req.params.project_id, (err, result) => {
+        if (err) {
+          next(err);
+        } else {
+          if (result) {
+            data.hasCurrentSprint = true;
+            queries.sprints.getSprintStates(result.id, (err2, result2) => {
+              if (err2) {
+                next(err);
+              } else {
+                data.sprintId = result.id;
+                data.sprintName = result.title;
+                data.states = result2;
+                res.render('project.hbs', data);
+              }
+            });
           } else {
-            data.sprintId = result.id;
-            data.sprintName = result.title;
-            data.states = result2;
             res.render('project.hbs', data);
           }
-        });
-      } else {
-        res.render('project.hbs', data);
-      }
+        }
+      });
     }
   });
 };
